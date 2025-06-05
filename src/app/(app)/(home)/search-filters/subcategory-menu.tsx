953bfe925f75564d096ad4c32c259e93bbcd285a
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-06-06 02:05:00 +0700

    06: categories-finalization

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,8 +1,9 @@
 import { Category } from "@/payload-types";
 import Link from "next/link";
+import { CustomCategory } from "../types";
 
 interface Props {
-  category: Category;
+  category: CustomCategory;
   isOpen: boolean;
   position: { top: number; left: number };
 }
@@ -30,10 +31,10 @@ export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
       >
         <div>
           {category.subcategories?.map((subcategory: Category) => (
-            <Link 
-            key={subcategory.slug} 
-            href="/"
-            className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
+            <Link
+              key={subcategory.slug}
+              href={`/${category.slug}/${subcategory.slug}`}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
             >
               {subcategory.name}
             </Link>
