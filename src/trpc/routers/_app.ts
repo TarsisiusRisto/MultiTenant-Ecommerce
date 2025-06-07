c77daf8a2766da2cd5e2683f754501849a44942b
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-06-08 02:07:29 +0700

    08: authentication

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from "../init";
 
 import { categoriesRouter } from "@/modules/categories/server/procedure";
+import { authRouter } from "@/modules/auth/server/procedure";
 
 export const appRouter = createTRPCRouter({
+  auth: authRouter,
   categories: categoriesRouter,
 });
 // export type definition of API
